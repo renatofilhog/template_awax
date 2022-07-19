05f9f2fe76ace848e96c34ca6bc79f4904dc6bf8
Renato Filho <frenato258@gmail.com>
2022-07-18 21:50:51 -0300

    finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,39 @@
+let pos = 0;
+let arr = document.querySelectorAll("#headerBanner .bol-banner");
+let totalSlides = arr.length;
+function passaSlide(){
+    let mPos = 0;
+    if(pos==0){
+        pos = 1;
+        mPos = pos*100;
+        document.getElementById("slideHeader").style.marginLeft = "-"+mPos+"vw";
+        retiraActive(1);
+        arr[pos].classList.add("active");
+        pos++;
+    } else if(pos >= totalSlides){
+        pos = 0;
+        document.getElementById("slideHeader").style.marginLeft = "-"+mPos+"vw";
+        retiraActive(1);
+        arr[pos].classList.add("active");
+    } else {
+        mPos = pos*100;
+        document.getElementById("slideHeader").style.marginLeft = "-"+mPos+"vw";
+        retiraActive(1);
+        arr[pos].classList.add("active");
+        pos++;
+    }
+    
+}
+setInterval(passaSlide,4000);
+
 document.querySelectorAll("#headerBanner .bol-banner").forEach((item)=>{
     item.addEventListener("click",()=>{
-        let pos = item.getAttribute("data-pos");
+        pos = item.getAttribute("data-pos");
         let marginPos = pos*100;
         document.getElementById("slideHeader").style.marginLeft = "-"+marginPos+"vw";
         retiraActive(1);
         item.classList.add("active");
+        pos++;
     });
 });
 
